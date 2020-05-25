4b9b740995225e493c7cdfc5067802bfb1a1fd9f
Fenngchenn <fcfight@163.com>
2020-05-25 17:53:30 +0800

    网络请求方式一

--- src/views/user/UserInfo.vue
+++ src/views/user/UserInfo.vue
@@ -4,19 +4,30 @@
     <div class="title">
       我的个人档案----{{$route.params.name}}
       <el-button type="text" @click="logout">退出登录</el-button>
+      <el-button type="primary" @click="jumpto">跳转到百度</el-button>
+    </div>
+    <div>
+      <ul>
+        <li v-for="item in list" :key="item.aid">
+          {{item.title}}
+        </li>
+      </ul>
     </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: "UserInfo",
   data () {
     return {
       loading: true,
       wishlist: ['candy', 'coffee'],
-      currentUser: this.$localStorage.get('currentUser')
+      currentUser: this.$localStorage.get('currentUser'),
+      list: []
     };
   },
   mounted() {
@@ -34,6 +45,15 @@ export default {
     logout() {
       this.$localStorage.remove('currentUser')
       this.$router.push('/login')
+    },
+    jumpto() {
+      const api = 'http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=1';
+      axios.get(api).then(res => {
+        console.log(res.data.result)
+        this.list = res.data.result
+      }).catch(err => {
+        console.log(err)
+      })
     }
   }
 }
