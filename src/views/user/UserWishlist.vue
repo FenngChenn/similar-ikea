17ae49cc82ac203bc51a9628ea7a11c8f3412b4d
Fenngchenn <fcfight@163.com>
2020-09-04 17:47:07 +0800

    wishlist-2

--- src/views/user/UserWishlist.vue
+++ src/views/user/UserWishlist.vue
@@ -3,7 +3,18 @@
   <div class="wish-list">
     <h1>心愿单</h1>
     <div class="todo-list">
-      <input type="text" class />
+      <input
+        type="text"
+        class="add-todo"
+        v-model="product"
+        placeholder="来这里记录你心仪的产品吧！"
+        @keyup.enter="addTodo"
+      />
+      <user-wishlist-item
+        v-for="(item, index) in wishlist"
+        :key="index"
+        :currentItem="item"
+      ></user-wishlist-item>
     </div>
     <!-- <el-button @click="test()">test</el-button>
     <el-button type="primary" @click="broadcast()">给shopbag组件广播数据</el-button>-->
@@ -12,12 +23,19 @@
 
 <script>
   import VueEvent from '@/components/content/util/VueEvent.js'
+  import UserWishlistItem from './UserWishlistItem'
 
   export default {
     name: 'UserWishlist',
+    components: {
+      UserWishlistItem,
+    },
     data() {
       return {
         msg: '哈哈哈哈哈',
+        product: '',
+        id: 0,
+        wishlist: [],
       }
     },
     props: ['test'],
@@ -28,6 +46,14 @@
           resolve('发出去了')
         }).then((res) => {
           console.log(res)
+        })
+      },
+      addTodo() {
+        if (this.product == '') return
+        this.wishlist.unshift({
+          id: this.id++,
+          content: this.product,
+          completed: false,
         })
       },
     },
@@ -46,5 +72,11 @@
       margin: 0 auto
       padding: 10px
       background-color: #fff
-      box-shadow: 0 0 5px #666
+      box-shadow: 1px 0 5px #666
+
+      .add-todo
+        width: 100%
+        padding: 10px 10px 10px 20px
+        border: none
+        outline: none
 </style>
---
+++ src/views/user/UserWishlistItem.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="list-item">
+    <input type="checkbox" v-model="currentItem.completed" />
+    <label>{{currentItem.content}}</label>
+    <button @click="deleteItem"></button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'UserWishlistItem',
+    props: {
+      currentItem: {
+        type: Object,
+      },
+    },
+    methods: {
+      deleteItem() {
+        this.$emit('deleteItem', this.currentItem.id)
+      },
+    },
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import '~assets/css/theme.styl'
+
+  .list-item
+    padding: 10px
+    font-weight: 600
+    font-size: 18px
+    border-top: 1px solid rgba(0, 0, 0, 0.1)
+    display: flex
+    justify-content: space-between
+
+    &:hover
+      button:after
+        content: 'x'
+        color: $red
+
+    input
+      clearStyle()
+      width: 50px
+
+      &:after
+        content: url('~assets/img/unchecked.svg')
+
+      &:checked:after
+        content: url('~assets/img/checked.svg')
+
+    label
+      flex: 1
+
+    button
+      clearStyle()
+      background-color: transparent
+      cursor: pointer
+</style>
